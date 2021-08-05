9c8da57c623bbb06fd399a0bc1d5ae615ee4b96b
jZtrK <efrain.cordova@utec.edu.pe>
2021-08-05 03:27:48 -0500

    added query proccesing

---
+++ src/parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jztrk on 8/4/21.
+//
+
+#include "parser.h"
---
+++ src/parser.h
@@ -0,0 +1,14 @@
+//
+// Created by jztrk on 8/4/21.
+//
+
+#ifndef SRC_PARSER_H
+#define SRC_PARSER_H
+
+
+class parser {
+
+};
+
+
+#endif //SRC_PARSER_H
